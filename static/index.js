741e7eaf08fb3dd25a65dee4bbdcef0688f916d3
yzhldt <103425482+yzhldt@users.noreply.github.com>
2022-04-12 20:07:24 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,11 +465,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '嘎？';
+        if (cps <= 5) return '嘎';
         if (cps <= 8) return '嘎嘎嘎';
         if (cps <= 10)  return '嘎嘎嘎嘎';
         if (cps <= 15) return '嘎嘎嘎嘎嘎';
-        return '人？';
+        return '嘎？';
     }
 
     function toStr(obj) {
